a23972effd33b2186253698194c3806f8deb4479
Jennifer Oxelson <oxelson@ucar.edu>
2022-07-12 17:42:41 -0600

    jdk 8 --> 11

--- packer/provisioners/scripts/bootstrap_last_aws.sh
+++ packer/provisioners/scripts/bootstrap_last_aws.sh
@@ -7,6 +7,6 @@ if [ "$EUID" -ne 0 ]; then
     SUDO_CMD="sudo"
 fi
 
-# Need openjdk-8-jre-headless so that jenkins can connect to worker nodes
+# Need openjdk-11-jre-headless so that jenkins can connect to worker nodes
 # using this ami.
-${SUDO_CMD} apt install --yes openjdk-8-jre-headless
+${SUDO_CMD} apt install --yes openjdk-11-jre-headless
